fee07600bd9d7dd2ffaf5b7f63838221231929b2
Rachid Rakaa <rachid1982fsb@gmail.com>
2019-09-29 06:12:02 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -22,12 +22,13 @@ def third_challenge
    :capulet => {
       :patriarch => {
         #your key/value pairs here
-        :name => "Romeo",
-        :age => "15"
-        R
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :matriarch => {
         #your key/value pairs here
+        :name => "Lord Capulet",
+        :age => "50"
         },
       :heroine => {
         #your key/value pairs here
